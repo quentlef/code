cc50b547435a120cba2e1d4615382d2a7e9324c2
quentlef <112124237+quentlef@users.noreply.github.com>
2024-01-02 12:23:42 +0100

    Update classe.py

--- classe.py
+++ classe.py
@@ -39,37 +39,79 @@ class Recherche:
         self.point_recherche_requis = point_recherche_requis   
 
 class Unite:  
+    list_Unite = []
+    def attaquer(self, cible):
+    #regarde la portée
+        if self.portee >= 2:
+        
+            cible.pv -= self.degats
+        
+            if cible.pv < 0:
+                print(f"Attaque réussie ! l'{cible.nom} est morte ")
+                del cible
+            else :
+                print(f"Attaque réussie ! Points de vie restants de la cible : {cible.pv}")
+            
+        else:
+            print("La cible est hors de portée !")      
     class Archer:
         # Attributs de classe partagés
+        
         degats = 10
         portee = 2
-        coutprod = 10
+        coutprod = 5
+        pm = 2
+        pv = 30
+        xp = 0
+        attaque = 1
+        valeur = 20
+
+        def __init__(self,joueur,nom,coord =(1,1)):
+            # Attributs d'instance propres à chaque instance
+            self.nom = nom     
+            self.coord = coord
+            self.joueur = joueur
+            Unite.list_Unite.append(self)
+            
+    class Eclaireur: 
+        # Attributs de classe partagés
+        
+        degats = 5
+        portee = 1
+        coutprod = 5
+        pm = 4
+        pv = 30
+        xp = 0
+        attaque = 1
+        valeur = 20
+        def __init__(self,joueur,nom,coord =(1,1)):
+            # Attributs d'instance propres à chaque instance
+            self.nom = nom     
+            self.coord = coord
+            self.joueur = joueur
+            Unite.list_Unite.append(self)
+            
+    class Guerrier: 
+        # Attributs de classe partagés
+        
+        degats = 10
+        portee = 1
+        coutprod = 5
         pm = 2
         pv = 50
         xp = 0
         attaque = 1
-
-        def __init__(self, nom,coord =(1,1)):
+        valeur = 30
+        def __init__(self,joueur, nom,coord =(1,1)):
             # Attributs d'instance propres à chaque instance
             self.nom = nom     
             self.coord = coord
-            
-        def attaquer(self, cible):
-        #regarde la portée
-            if self.portee >= 2:
-            
-                cible.pv -= self.degats
-            
-                if cible.pv < 0:
-                    print(f"Attaque réussie ! l'{cible.nom} est morte ")
-                    del cible
-                else :
-                    print(f"Attaque réussie ! Points de vie restants de la cible : {cible.pv}")
-                
-            else:
-                print("La cible est hors de portée !")        
+            self.joueur = joueur
+            Unite.list_Unite.append(self)
 
-                     
+
+
+                    
 #Initialisation des cases 
 class Case:
     def __init__(self, nom, occtroit:[] , bonusprox , caractéristique, etatdeplacement):
@@ -86,4 +128,20 @@ class Case_plateau:
         self.typecase = typecase
         self.coord = coord
         Case_plateau.list_cases.append(self)
-
+        
+j1 = Joueur("J1")
+j2 = Joueur("J2")
+            
+class ValeurUnite:
+    def __init__(self):
+        self.NbrUniteJ1 = 0
+        self.ValeurUniteJ1 = 0
+        self.NbrUniteJ2 = 0
+        self.ValeurUniteJ2 = 0
+    def ChangeValeur(self,unite):
+        if unite.joueur == j1:
+            self.NbrUniteJ1 += 1
+            self.ValeurUniteJ1 += unite.valeur
+        if unite.joueur == j2:
+            self.NbrUniteJ2 += 1
+            self.ValeurUniteJ2 += unite.valeur
